5b5025c0e6a58fe7b423620f389c6694c6ab1bec
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-12-14 15:19:51 +0400

    start typescript

---
+++ typescript.js
@@ -0,0 +1,17 @@
+// console.log("Ali")
+// let a : string;
+// a="Nacaf";
+// console.log(a)
+// const a:Array<number> = [1,4,51,86]
+// console.log(...a)
+function addNum(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNum(12, 54));
+function addnumber(num1, num2) {
+    if (num2 == undefined) {
+        return "First num: " + num1;
+    }
+    return "sum: " + (num1 + num2);
+}
+console.log(addnumber(13, 41));
