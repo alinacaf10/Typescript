dcf98883460515d934ef65a64320b8923b7f255a
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-12-14 15:46:46 +0400

    add class

--- typescript.js
+++ typescript.js
@@ -4,14 +4,28 @@
 // console.log(a)
 // const a:Array<number> = [1,4,51,86]
 // console.log(...a)
-function addNum(num1, num2) {
-    return num1 + num2;
-}
-console.log(addNum(12, 54));
-function addnumber(num1, num2) {
-    if (num2 == undefined) {
-        return "First num: " + num1;
+// function addNum(num1:number,num2:number){
+//     return num1+num2;
+// }
+// console.log(addNum(12,54))
+// function addnumber(num1:number,num2?:number){
+//     if (num2==undefined) {
+//         return "First num: "+num1
+//     }
+//     return "sum: "+(num1+num2)
+// }
+// console.log(addnumber(13,41))
+var Personal = /** @class */ (function () {
+    function Personal(name, age, phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        console.log("Personal was created");
     }
-    return "sum: " + (num1 + num2);
-}
-console.log(addnumber(13, 41));
+    Personal.prototype.showInfos = function () {
+        console.log("Name: ".concat(this.name, "\nAge: ").concat(this.age, "\nPhone: ").concat(this.phone));
+    };
+    return Personal;
+}());
+var names = new Personal("Ali", 22, "775480446");
+names.showInfos();
--- typescript.ts
+++ typescript.ts
@@ -6,16 +6,36 @@
 
 // const a:Array<number> = [1,4,51,86]
 // console.log(...a)
-function addNum(num1:number,num2:number){
-    return num1+num2;
-}
-console.log(addNum(12,54))
+// function addNum(num1:number,num2:number){
+//     return num1+num2;
+// }
+// console.log(addNum(12,54))
 
-function addnumber(num1:number,num2?:number){
-    if (num2==undefined) {
-        return "First num: "+num1
+// function addnumber(num1:number,num2?:number){
+//     if (num2==undefined) {
+//         return "First num: "+num1
+//     }
+//     return "sum: "+(num1+num2)
+// }
+
+// console.log(addnumber(13,41))
+
+class Personal{
+    name:string
+    age:number
+    phone:string
+    constructor(name:string,age:number,phone:string){
+        this.name=name
+        this.age=age
+        this.phone=phone
+    console.log("Personal was created")
+
     }
-    return "sum: "+(num1+num2)
+    showInfos(){
+        console.log(`Name: ${this.name}\nAge: ${this.age}\nPhone: ${this.phone}`)
+    }
+
 }
 
-console.log(addnumber(13,41))
+const names=new Personal("Ali",22,"775480446")
+names.showInfos()
